0b70f5dcb515a5d7a5faceab55618afff6ce79d4
Kevin Sung <kevin.sung.123@gmail.com>
2019-02-25 13:50:20 -0500

    Updates

---
+++ precache-manifest.2a2894b84b8734e245c3ea39bd335201.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ff373a1c9a4c164da0",
+    "url": "/solarpix/static/js/runtime~main.72ff373a.js"
+  },
+  {
+    "revision": "85d10a4a9c738c8b14d5",
+    "url": "/solarpix/static/js/main.85d10a4a.chunk.js"
+  },
+  {
+    "revision": "dde6fa37968872184528",
+    "url": "/solarpix/static/js/2.dde6fa37.chunk.js"
+  },
+  {
+    "revision": "cec30b621e52fdfe064dab25d0779dba",
+    "url": "/solarpix/index.html"
+  }
+];
