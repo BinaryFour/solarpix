bd2551b5e998cb29410fb9f28860970334110c7e
binaryfour <kevin.sung.123@gmail.com>
2019-04-16 13:47:16 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/solarpix/precache-manifest.244f8d4ca19397924a0ffc6946cf25ad.js"
+  "/solarpix/precache-manifest.3127a581b5fbfe3abf1a995404a90c4e.js"
 );
 
 workbox.clientsClaim();
