345e259e11f6c071eecda9fac6818025f9302f27
Kevin Sung <kevin.sung.123@gmail.com>
2019-06-13 18:06:20 -0400

    Updates

--- precache-manifest.1dd8ed8a7558db89b23cd66e57078602.js
+++ precache-manifest.1dd8ed8a7558db89b23cd66e57078602.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/solarpix/static/js/runtime~main.27a004bf.js"
   },
   {
-    "revision": "1565a4ce983a9489117d",
-    "url": "/solarpix/static/js/main.cf492c24.chunk.js"
+    "revision": "8b229ff4a7cf6d1fcf0a",
+    "url": "/solarpix/static/js/main.adf7371c.chunk.js"
   },
   {
     "revision": "cc497b3607ef154652ca",
     "url": "/solarpix/static/js/2.8a58d515.chunk.js"
   },
   {
-    "revision": "4492dcc21f72f8246947638383e8c3bf",
+    "revision": "a90e0d6cc1234f8ebd74fa2228856305",
     "url": "/solarpix/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/solarpix/precache-manifest.8d82bdbd5fa50c1aa1b76a3d1aa9fe2d.js"
+  "/solarpix/precache-manifest.1dd8ed8a7558db89b23cd66e57078602.js"
 );
 
 workbox.clientsClaim();
